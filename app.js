cd079dd8cbaad92404435c67fce5a64a5420b09a
Wami Wamisoftware <wami@Wamis-MacBook-Pro.local>
2020-08-10 10:23:11 +0300

    add handlebars

--- app.js
+++ app.js
@@ -2,13 +2,21 @@ const path = require('path')
 
 const express = require('express')
 const bodyParser = require('body-parser')
+const expressHbs = require('express-handlebars')
 
 const app = express()
 
-app.set('view engine', 'pug')
+app.engine(
+  'hbs',
+  expressHbs({
+    layoutsDir: 'views/layouts/',
+    defaultLayout: 'main-layout',
+    extname: 'hbs'
+  })
+)
+app.set('view engine', 'hbs')
 app.set('views', 'views')
 
-// owner files
 const adminData = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 
@@ -18,9 +26,8 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use('/admin', adminData.routes)
 app.use(shopRoutes)
 
-app.use((req, res) => {
-  res.status(404).render('404', { docTitle: 'Page Not Found!'})
+app.use((req, res, next) => {
+  res.status(404).render('404', { pageTitle: 'Page Not Found' })
 })
 
-const PORT = process.env.PORT
-app.listen(PORT || 3000)
+app.listen(3000)
--- routes/admin.js
+++ routes/admin.js
@@ -1,21 +1,26 @@
-const path = require('path')
+// const path = require('path')
 
 const express = require('express')
 
-const rootDir = require('../util/path')
+// const rootDir = require('../util/path')
 
 const router = express.Router()
 
 const products = []
-/**
- * {@param pathname} '/admin' + '/....' 
- */
 
-router.get('/add-product', (req, res) => {
-  res.render('add-product', { docTitle: 'Add product', path: '/admin/add-product' })
+// /admin/add-product => GET
+router.get('/add-product', (req, res, next) => {
+  res.render('add-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
+    formsCSS: true,
+    productCSS: true,
+    activeAddProduct: true
+  })
 })
 
-router.post('/add-product', (req, res) => {
+// /admin/add-product => POST
+router.post('/add-product', (req, res, next) => {
   products.push({ title: req.body.title })
   res.redirect('/')
 })
